21d56b9eab434b1a8a0a8adec721a77ac228a346
albernazdepijama <deboraalbernaz2013@gmail.com>
2026-06-15 19:01:55 -0300

    atualização leve

--- programacao01pyton.py
+++ programacao01pyton.py
@@ -2,7 +2,7 @@ nome = "Débora" #str - string
 ultimo_nome = "Samara ALbernaz Silva" # str- string
 idade = 23 # int - inteiro
 altura = 1.62 #float - ponto flutuante
-maior_de_idade = True # bool - boolean
+maior_de_idade = True # bool - boolean true/false
 print("hola ", nome, ultimo_nome, "voce tem", idade, "anos, sua altura e", altura, "voce e maior de idade?", maior_de_idade)
 
 
