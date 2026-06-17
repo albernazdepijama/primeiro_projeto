b6a639ba19bac00c3df622ab472db43eb89b9110
albernazdepijama <deboraalbernaz2013@gmail.com>
2026-06-17 18:50:14 -0300

    Estruta condicional e média de aluno utilizando variações lógicas if else e elif

--- media_aluno.py
+++ media_aluno.py
@@ -10,5 +10,14 @@ if media >= 7 :
     print(f"Aluno(a) Aprovado(a) com media {media:.2f}")
 elif media > 3 and media < 7:
     print(f"Aluno em Recuperação {media:.2f}")
+    fez_recuperacao = input("Aluno ja fez a recuperação? s/n: ")
+    if fez_recuperacao == "s":
+        nota_recuperacao = float(input("Digite a nota da recuperação: "))
+        if nota_recuperacao >= 5:
+            print("Aluno(a) aprovado (a) pela recuperação")
+        else:
+            print("Aluno(a) não atingiu a nota necessaria apos a recuperação! ")
+    else:
+        print("Aluno (A) ainda não fez a recuperação")
 else:
     print(f"Aluno(a) Reprovado(a) com media {media:.2f}")
